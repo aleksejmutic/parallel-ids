995a0b50d2638d518c34aebb43582460a03f564f
alexei <aleksej.mutic@gmail.com>
2026-08-16 14:21:03 +0200

    Events are now sent from producers to Kafka and then read from consumers.

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
+import threading
+
+from consumer.event_consumer import EventConsumer
 from sources.ssh.ssh_runner import run_ssh
 
 
+def run_consumer():
+
+    consumer = EventConsumer()
+
+    try:
+        for event in consumer.consume():
+            print("CONSUMED EVENT:")
+            print(event)
+
+    finally:
+        consumer.close()
+
+
 def main():
-    run_ssh()
+
+    ssh_thread = threading.Thread(
+        target=run_ssh,
+        daemon=True
+    )
+
+    consumer_thread = threading.Thread(
+        target=run_consumer,
+        daemon=True
+    )
+
+    ssh_thread.start()
+    consumer_thread.start()
+
+    ssh_thread.join()
+    consumer_thread.join()
 
 
 if __name__ == "__main__":
